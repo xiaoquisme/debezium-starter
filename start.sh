206ee4fbaf4a5cd22b6e4c391a38b06f9a6041b2
XiaoQu <1483523635@qq.com>
2024-05-18 23:15:29 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-docker run --link kafka:kafka -d -p 8081:8080 \
--e KAFKA_CLUSTERS_0_NAME=local \
--e KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS='kafka:9092' \
--e KAFKA_BROKERCONNECT='kafka:9092' \
--e DYNAMIC_CONFIG_ENABLED=true \
-provectuslabs/kafka-ui
